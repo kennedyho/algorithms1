7c9e0e3f98e58070b1e17b4630707d57720e0ec7
Kennedy Ho <kennedyho0812@gmail.com>
2019-11-06 01:28:26 +0900

    Refactor the whole code to use WQUF library instead of self made WQUF functions

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -7,10 +7,12 @@
  *
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    private int[] grid;    // the model of the n-by-n grid
-    private int[] size;     // size of the tree for Weighted Union Find
-    private final int sitesPerRow;          // keep track of the number of grid per row (n)
+    private boolean[] grid;    // the model of the n-by-n grid
+    private WeightedQuickUnionUF wquf; // Weighted Quick Union Find object
+    private final int sitesPerRow; // keep track of the number of grid per row (n)
     private int numberOfOpenSites; // keep track of the number of sites that are opened
 
     // creates n-by-n grid, with all sites initially blocked
@@ -19,12 +21,13 @@ public class Percolation {
             throw new IllegalArgumentException("n = " + Integer.toString(n));
         }
         this.sitesPerRow = n;
-        grid = new int[n * n];
-        size = new int[n * n];
+        wquf = new WeightedQuickUnionUF(n * n + 1); // with 1 extra virtual site
+        grid = new boolean[n * n];
         numberOfOpenSites = 0;
-        for (int i = 0; i < n * n; i++) {
-            grid[i] = i;
-            size[i] = 0;
+
+        // connect top rows to virtual site n*n,
+        for (int i = 0; i < n; i++) {
+            wquf.union(i, n * n);
         }
     }
 
@@ -35,27 +38,27 @@ public class Percolation {
                     "Row: " + Integer.toString(row) + "\tColumn: " + Integer.toString(col));
         }
         int index = (col - 1) + ((row - 1) * sitesPerRow);
-        if (size[index] == 0) {
-            size[index] = 1; // change the size to 1 to indicate an opened site
+        if (!grid[index]) {
+            grid[index] = true; // change the size to 1 to indicate an opened site
 
             // If not top row, union with the open site above
             if (row != 1 && isOpen(row - 1, col)) {
-                union(index, index - sitesPerRow);
+                wquf.union(index, index - sitesPerRow);
             }
 
             // If not bottom row, union with the open site below
             if (row != sitesPerRow && isOpen(row + 1, col)) {
-                union(index, index + sitesPerRow);
+                wquf.union(index, index + sitesPerRow);
             }
 
             // If not leftmost column, union with the open left site
             if (col != 1 && isOpen(row, col - 1)) {
-                union(index, index - 1);
+                wquf.union(index, index - 1);
             }
 
             // If not rightmost column, union with the open right site
             if (col != sitesPerRow && isOpen(row, col + 1)) {
-                union(index, index + 1);
+                wquf.union(index, index + 1);
             }
             numberOfOpenSites++;
         }
@@ -68,10 +71,8 @@ public class Percolation {
                     "Row: " + Integer.toString(row) + "\tColumn: " + Integer.toString(col));
         }
         int index = (col - 1) + ((row - 1) * sitesPerRow);
-        if (size[index] == 0) {
-            return false;
-        }
-        return true;
+
+        return grid[index];
     }
 
     // is the site (row, col) full?
@@ -87,13 +88,8 @@ public class Percolation {
 
         int index = (col - 1) + ((row - 1) * sitesPerRow);
 
-        // checks if any of the sites in first row is connected to this site
-        for (int i = 0; i < sitesPerRow; i++) {
-            if (connected(i, index)) {
-                return true;
-            }
-        }
-        return false;
+        // checks if this site is connected to the virtual site on top
+        return wquf.connected(index, grid.length);
     }
 
     // returns the number of open sites
@@ -111,34 +107,5 @@ public class Percolation {
             }
         }
         return false;
-    }
-
-    // find the root of a site
-    private int root(int i) {
-        while (i != grid[i]) {
-            grid[i] = grid[grid[i]];
-            i = grid[i];
-        }
-        return i;
-    }
-
-    // check if two sites are connected or not
-    private boolean connected(int p, int q) {
-        return root(p) == root(q);
-    }
-
-    // Weighted Quick Union with Path Compression
-    private void union(int p, int q) {
-        int i = root(p);
-        int j = root(q);
-        if (i == j) return;
-        if (size[i] < size[j]) {
-            grid[i] = j;
-            size[j] += size[i];
-        }
-        else {
-            grid[j] = i;
-            size[i] += size[j];
-        }
     }
 }
