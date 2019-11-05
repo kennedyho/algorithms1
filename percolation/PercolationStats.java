1d6b0e5b8ce85e1d983bef82a0686825801656a6
Kennedy Ho <kennedyho0812@gmail.com>
2019-11-06 00:37:37 +0900

    Change percolation object to a local variable to prevent holding up big percolation object that takes up memory

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -13,7 +13,6 @@ import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
     private static final double CONFIDENCE = 1.96; // confidence constant for calculation
 
-    private Percolation percolation; // Object of the class Percolation
     private double[] percolationThresholds; // an array of percolation thresholds
     private final int numberOfSitesPerRow; // number of sites per row entered by user
     private double meanVal; // sample mean value of the percolation thresholds
@@ -62,13 +61,13 @@ public class PercolationStats {
     // do multiple experiments based on the number of trials
     private void doMultipleExperiments() {
         for (int i = 0; i < percolationThresholds.length; i++) {
-            percolation = new Percolation(numberOfSitesPerRow);
-            percolationThresholds[i] = getPercolationThreshold();
+            Percolation percolation = new Percolation(numberOfSitesPerRow);
+            percolationThresholds[i] = getPercolationThreshold(percolation);
         }
     }
 
     // do the percolation experiment and get threshold
-    private double getPercolationThreshold() {
+    private double getPercolationThreshold(Percolation percolation) {
         while (!percolation.percolates()) {
             int row = StdRandom.uniform(getNumberOfSitesPerRow()) + 1;
             int col = StdRandom.uniform(getNumberOfSitesPerRow()) + 1;
