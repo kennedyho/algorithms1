5755069d7633eb15d99b62f7cb93c94cf695b847
Kennedy Ho <kennedyho0812@gmail.com>
2019-11-06 03:31:25 +0900

    Still contain backwash problem

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -10,10 +10,12 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private final int topVirtualSiteIndex; // index of the top virtual site
+    private final int bottomVirtualSiteIndex; // index of the bottom virtual site
     private boolean[] grid;    // the model of the n-by-n grid
     private final WeightedQuickUnionUF wquf; // Weighted Quick Union Find object
     private final int sitesPerRow; // keep track of the number of grid per row (n)
-    private int numberOfOpenSites; // keep track of the number of sites that are opened
+    private int numberOfOpenSites; // counter to keep track of the number of open sites
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -21,15 +23,12 @@ public class Percolation {
             throw new IllegalArgumentException("n = " + Integer.toString(n));
         }
         this.sitesPerRow = n;
-        wquf = new WeightedQuickUnionUF(n * n + 2); // with 1 extra virtual site
-        grid = new boolean[n * n];
+        wquf = new WeightedQuickUnionUF(
+                sitesPerRow * sitesPerRow + 2); // with 2 extra virtual sites
+        grid = new boolean[sitesPerRow * sitesPerRow];
+        topVirtualSiteIndex = sitesPerRow * sitesPerRow;
+        bottomVirtualSiteIndex = sitesPerRow * sitesPerRow + 1;
         numberOfOpenSites = 0;
-
-        // connect top rows to virtual site n*n,
-        for (int i = 0; i < n; i++) {
-            wquf.union(i, n * n);
-            wquf.union(i + n * n - n, n * n + 1);
-        }
     }
 
     // opens the site (row, col) if it is not open already
@@ -38,8 +37,8 @@ public class Percolation {
             throw new IllegalArgumentException(
                     "Row: " + Integer.toString(row) + "\tColumn: " + Integer.toString(col));
         }
-        int index = (col - 1) + ((row - 1) * sitesPerRow);
-        if (!grid[index]) {
+        int index = getIndex(row, col);
+        if (!isOpen(row, col)) {
             grid[index] = true; // change the size to 1 to indicate an opened site
 
             // If not top row, union with the open site above
@@ -47,9 +46,17 @@ public class Percolation {
                 wquf.union(index, index - sitesPerRow);
             }
 
+            if (row == 1) { // if top row, connect to top virtual site
+                wquf.union(index, topVirtualSiteIndex);
+            }
+
             // If not bottom row, union with the open site below
             if (row != sitesPerRow && isOpen(row + 1, col)) {
                 wquf.union(index, index + sitesPerRow);
+            }
+
+            if (row == sitesPerRow) { // if bottom row, connect to bottom virtual site
+                wquf.union(index, bottomVirtualSiteIndex);
             }
 
             // If not leftmost column, union with the open left site
@@ -71,9 +78,8 @@ public class Percolation {
             throw new IllegalArgumentException(
                     "Row: " + Integer.toString(row) + "\tColumn: " + Integer.toString(col));
         }
-        int index = (col - 1) + ((row - 1) * sitesPerRow);
 
-        return grid[index];
+        return grid[getIndex(row, col)];
     }
 
     // is the site (row, col) full?
@@ -87,10 +93,8 @@ public class Percolation {
             return false;
         }
 
-        int index = (col - 1) + ((row - 1) * sitesPerRow);
-
         // checks if this site is connected to the virtual site on top
-        return wquf.connected(index, grid.length);
+        return wquf.connected(getIndex(row, col), topVirtualSiteIndex);
     }
 
     // returns the number of open sites
@@ -100,6 +104,11 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return wquf.connected(grid.length, grid.length + 1);
+        return wquf.connected(topVirtualSiteIndex, bottomVirtualSiteIndex);
+    }
+
+    // map row and col to get array index
+    private int getIndex(int row, int col) {
+        return (col - 1) + ((row - 1) * sitesPerRow);
     }
 }
