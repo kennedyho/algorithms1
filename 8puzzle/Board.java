93bfedf0f22c63d5deedbe2474f252c48169d052
Kennedy Ho <kennedyho0812@gmail.com>
2019-12-10 00:17:12 +0900

    Implemented hamming and toString

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,9 +4,12 @@
  *  Description: Slider Puzzle assignment for Algorithms I Week 4 Assignment
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
 
-    private int[][] myBoard;
+    private int[][] myBoard;    // current board
+    private int N;              // dimension of board N
 
     /**
      * Constructor to create a board from an n-by-n array of tiles,
@@ -19,24 +22,25 @@ public class Board {
         }
 
         // If row and column are not of same length
-        for (int i = 0; i < tiles.length; i++) {
-            if (tiles.length != tiles[i].length) {
+        for (int row = 0; row < tiles.length; row++) {
+            if (tiles.length != tiles[row].length) {
                 throw new IllegalArgumentException("The board is not n*n!");
             }
 
             // If there is null element in the array
-            if (tiles[i] == null) {
+            if (tiles[row] == null) {
                 throw new NullPointerException("One of the rows is null!");
             }
         }
 
         // new instance of 2D array
         myBoard = new int[tiles.length][];
+        N = myBoard.length;
 
         // Copy the parameter array to myBoard
-        for (int i = 0; i < tiles.length; i++) {
-            for (int j = 0; i < tiles[i].length; j++) {
-                myBoard[i][j] = tiles[i][j];
+        for (int row = 0; row < tiles.length; row++) {
+            for (int col = 0; row < tiles[row].length; col++) {
+                myBoard[row][col] = tiles[row][col];
             }
         }
     }
@@ -45,15 +49,20 @@ public class Board {
      * @return string representation of this board
      */
     public String toString() {
-
-
+        StdOut.println(dimension());
+        for (int row = 0; row < myBoard.length; row++) {
+            for (int col = 0; col < myBoard[row].length; col++) {
+                StdOut.print(" " + formatWidth(myBoard[row][col], 5));
+            }
+            StdOut.println();
+        }
     }
 
     /**
      * @return board dimension n
      */
     public int dimension() {
-        return myBoard.length;
+        return N;
     }
 
     /**
@@ -62,7 +71,17 @@ public class Board {
      * @return number of tiles out of place
      */
     public int hamming() {
-
+        int outOfPosCnt = 0;
+        for (int row = 0; row < myBoard.length; row++) {
+            for (int col = 0; col < myBoard[row].length; col++) {
+                int correctPos = (row == N - 1 && col == N - 1) ? 0 :
+                                 (row * N + col + 1);
+                if (correctPos != myBoard[row][col]) {
+                    outOfPosCnt++;
+                }
+            }
+        }
+        return outOfPosCnt;
     }
 
     /**
@@ -108,6 +127,10 @@ public class Board {
      */
     public Board twin() {
 
+    }
+
+    private String formatWidth(int value, int width) {
+        return String.format("%" + "-" + width + "s" + value);
     }
 
     /**
